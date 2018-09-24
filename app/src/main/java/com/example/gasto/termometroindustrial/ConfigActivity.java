701d3b7eb7cc45431cd788351dd15c0e3b166c67
Gastón Bonanno <gastonsbonanno@users.noreply.github.com>
2018-09-24 00:15:48 -0300

    Agrego Gauge, y campo para setera el tiempo limite.

--- app/src/main/java/com/example/gasto/termometroindustrial/ConfigActivity.java
+++ app/src/main/java/com/example/gasto/termometroindustrial/ConfigActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 public class ConfigActivity extends AppCompatActivity{
 
     EditText loConfTemp;
+    EditText loConfTiempo;
     Button loBotonComenzar;
     Intent intent;
     @Override
@@ -19,6 +20,7 @@ public class ConfigActivity extends AppCompatActivity{
         setContentView(R.layout.activity_config);
 
         loConfTemp = (EditText) findViewById(R.id.lo_confTemp);
+        loConfTiempo = (EditText) findViewById(R.id.lo_confTiempo);
         loBotonComenzar = (Button) findViewById(R.id.lo_botonComenzar);
         loBotonComenzar.setOnClickListener(new onClickClass());
     }
@@ -27,16 +29,49 @@ public class ConfigActivity extends AppCompatActivity{
     public class onClickClass implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            try{
+            try{/*
                 intent = new Intent(ConfigActivity.this, MainActivity.class);
                 intent.putExtra("confTemp",new Integer(loConfTemp.getText().toString()));
-                startActivity(intent);
+                if(loConfTiempo.getText() != null){
+                    String sAux = loConfTiempo.getText().toString();
+                    if(Integer.parseInt(sAux)<= 0){
+                        intent.putExtra("confTiempo",new Integer(loConfTiempo.getText().toString()));
+                    }else{
+                        Toast.makeText(ConfigActivity.this, "Debe seleccionar una temperatura valida", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                */
+                if(validarPermiteModificar())
+                    startActivity(intent);
             }
             catch (Exception e) {
                 Toast.makeText(ConfigActivity.this, "Debe seleccionar una temperatura valida", Toast.LENGTH_SHORT).show();
             }
         }
 
+    }
+
+    public boolean validarPermiteModificar(){
+        intent = new Intent(ConfigActivity.this, MainActivity.class);
+        if(!loConfTemp.getText().toString().equals("")){
+            intent.putExtra("confTemp",new Integer(loConfTemp.getText().toString()));
+        }else{
+            Toast.makeText(ConfigActivity.this, "Debe seleccionar una temperatura", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(!loConfTiempo.getText().toString().equals("")){
+            String sAux = loConfTiempo.getText().toString();
+            if(Integer.parseInt(sAux)> 0){
+                intent.putExtra("confTiempo",new Integer(loConfTiempo.getText().toString()));
+            }else{
+                Toast.makeText(ConfigActivity.this, "Debe seleccionar una valor de tiempo válido", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+
+        return true;
     }
 
 
--- app/src/main/java/com/example/gasto/termometroindustrial/MainActivity.java
+++ app/src/main/java/com/example/gasto/termometroindustrial/MainActivity.java
@@ -5,24 +5,32 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.os.BatteryManager;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import de.nitri.gauge.Gauge;
+
 
 public class MainActivity extends AppCompatActivity {
 
     TextView loTemperatura;
+    TextView loTiempo;
 
     MediaPlayer alarm;
     Handler handler;
     Runnable runnable;
     int iTemperatura;
     Integer iConfTemp;
+    Integer iConfTiempo;
     Bundle bundle;
     Intent intent;
+    Gauge gauge;
+    CountDownTimer timer;
+    boolean estadoAlarma = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +38,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loTemperatura = (TextView)findViewById(R.id.lo_temperatura);
+        loTiempo = (TextView)findViewById(R.id.lo_tiempo);
+        gauge = (Gauge)findViewById(R.id.gauge);
         alarm= MediaPlayer.create(MainActivity.this,R.raw.alarm);
         intent = getIntent();
+
+
+
+
+
         try {
             bundle = intent.getExtras();
             int intAux = bundle.getInt("confTemp");
             iConfTemp = new Integer(intAux);
+            intAux = bundle.getInt("confTiempo");
+            iConfTiempo = new Integer(intAux);
+            setTimer(iConfTiempo*60*1000);
+            timer.start();
         }catch (NumberFormatException e){
             e.printStackTrace();
             Toast.makeText(MainActivity.this, "No es posible obtener la configuración", Toast.LENGTH_SHORT).show();
@@ -46,12 +65,16 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 iTemperatura = _getTemperaturaActual();
                 handler.postDelayed(runnable, 5000);
+                gauge.setValue(iTemperatura);
 
                 if(iConfTemp != null && iTemperatura < iConfTemp){
-                    alarm.start();
+                    estadoAlarma = true;
                     Toast.makeText(MainActivity.this, "La temperatura es muy baja", Toast.LENGTH_SHORT).show();
                 }
                 loTemperatura.setText("Temperatura actual: "+iTemperatura+"C°");
+                //loTiempo.setText("Tiempo restante: "+iConfTiempo+" Minutos");
+                if(estadoAlarma)
+                    iniciarAlarma();
             }
         };
         handler = new Handler();
@@ -65,9 +88,45 @@ public class MainActivity extends AppCompatActivity {
         return temperatura;
     }
 
+ /*      private int _getTiempoRestante(){
+        return tiempoRestante;
+    }
+*/
+
     public void onBackPressed() {
-        iConfTemp = -99; // Cambiar esto.
+        //iConfTemp = -99; // Cambiar esto.
+        estadoAlarma = false;
+        timer.cancel();
         finish();
     }
 
+/*
+    public class CountDownT extends CountDownTimer {
+        public CountDownT(long milisegundos, long TimeGap){
+            super.(milisegundos)
+        }
+
+    }
+*/
+
+
+    private void setTimer(Integer milisegundos){
+        timer = new CountDownTimer(milisegundos,1000) {
+            @Override
+            public void onTick(long l) {
+                //loTiempo.setText(Long.toString(l));
+                loTiempo.setText("Tiempo restante: "+l/60/1000+" Minutos");
+            }
+
+            @Override
+            public void onFinish() {
+                estadoAlarma = true;
+                Toast.makeText(MainActivity.this, "El temporizador finalizó", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    private void iniciarAlarma(){
+        alarm.start();
+    }
 }
